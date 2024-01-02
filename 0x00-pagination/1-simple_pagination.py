627be4559e394281bb3bcf1299e9da026aa31150
Charity K. Mutembei <charity.softwaredev@gmail.com>
2024-01-02 11:58:41 +0300

    task 1

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""
+task 2
+"""
+import csv
+from typing import List
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    check file 0
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return start_index, end_index
+
+
+class Server:
+    """
+    copied class
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert isinstance(page,
+                          int) and page > 0, "Pagemust be a positive integer"
+        assert isinstance(page_size,
+                          int) > 0, "Page size must be a positive integer"
+
+        start_index, end_index = index_range(page, page_size)
+        dataset = self.dataset()
+
+        if start_index >= len(dataset):
+            return []
+
+        return dataset[start_index:end_index]
