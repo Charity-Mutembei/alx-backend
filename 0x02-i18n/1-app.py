ae356e9801a2bcc66839d38fbbc4867b1e931cbe
Charity K. Mutembei <charity.softwaredev@gmail.com>
2024-01-10 03:00:50 +0300

    task 1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,30 +1,31 @@
 #!/usr/bin/env python3
-"""
-babel starts here
-"""
+'''Task 0: Basic Flask app
+'''
+
 from flask import Flask, render_template
 from flask_babel import Babel
 
-app = Flask(__name__)
-babel = Babel(app)
-
 
 class Config:
-    """
-    config class  for the languages
-    """
+    '''Config class'''
+
     LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-babel.init_app(app)
+app = Flask(__name__)
 app.config.from_object(Config)
-babel.default_locale = 'en'
-babel.default_timezone = 'UTC'
+app.url_map.strict_slashes = False
+
+babel = Babel(app)
 
 
 @app.route('/')
 def index():
-    """
-    task 1 function
-    """
-    return render_template('1-index.html')
+    '''default route'''
+    return render_template("1-index.html",)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
