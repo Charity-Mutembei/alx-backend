b098172109eb75915b0a7f47dd3c39b296c8e76c
Charity K. Mutembei <charity.softwaredev@gmail.com>
2024-01-10 03:34:24 +0300

    task 4

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""
+babel starts here
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """
+    config class for the languages
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Get the best-matching language for the user.
+    """
+    # Check if the 'locale' parameter is present in the request
+    locale_param = request.args.get('locale')
+
+    # If the 'locale' parameter is a supported language, use it
+    if locale_param and locale_param in app.config['LANGUAGES']:
+        return locale_param
+
+    # Resort to the previous default behavior
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """
+    task 1 function
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
