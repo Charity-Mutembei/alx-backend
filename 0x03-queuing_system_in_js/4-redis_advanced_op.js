ce8c6aebe40d3fbbbc16abe8a78d0e73b1ee4e50
Charity-Mutembei <charity.softwaredev@gmail.com>
2024-06-27 00:45:40 +0300

    task 2

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-magic-numbers */
+import redis from 'redis';
+
+const client = redis.createClient({
+  'host': '127.0.0.1',
+  'port': 6379
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.error('Redis client not connected to the server:', err.message);
+});
+
+// Create Hash
+client.hset('HolbertonSchools', 'Portland', 50, redis.print);
+client.hset('HolbertonSchools', 'Seattle', 80, redis.print);
+client.hset('HolbertonSchools', 'New York', 20, redis.print);
+client.hset('HolbertonSchools', 'Bogota', 20, redis.print);
+client.hset('HolbertonSchools', 'Cali', 40, redis.print);
+client.hset('HolbertonSchools', 'Paris', 2, redis.print);
+
+// Display Hash
+client.hgetall('HolbertonSchools', (err, reply) => {
+  if (err) {
+    console.error('Error fetching hash:', err);
+  } else {
+    console.log(reply);
+  }
+});
