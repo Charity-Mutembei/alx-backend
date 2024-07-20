e22f3e7df2870820a866eee78c98175aae525e84
Charity-Mutembei <charity.softwaredev@gmail.com>
2024-07-20 15:50:20 +0300

    task 0

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """
-1. Simple pagination
+Write a function named index_range that
+takes two integer arguments page and page_size.
 """
+
 import csv
 from typing import List
 
@@ -9,7 +11,7 @@ from typing import List
 def index_range(page: int, page_size: int) -> tuple:
     """
     Returns a tuple of size two containing a start
-    index and an end index
+    index and an end index.
     """
     first_index = (page - 1) * page_size
     last_index = first_index + page_size
@@ -17,33 +19,30 @@ def index_range(page: int, page_size: int) -> tuple:
 
 
 class Server:
-    """
-    copied class
-    """
+    """Server class to paginate a database of popular baby names."""
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
-    def dataset(self) -> List[List]:
+    def dataset(self) -> List[List[str]]:
+        """Cached dataset."""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
-            self.__dataset = dataset[1:]
-
+            self.__dataset = dataset[1:]  # Skip header
         return self.__dataset
 
-    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        assert isinstance(page,
-                          int) and page > 0, "must be positive integer"
-        assert isinstance(page_size,
-                          int) > 0, "must be positive integer"
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List[str]]:
+        """Return the appropriate page of dataset."""
+        assert isinstance(page, int) and page > 0, "Page must be a positive integer."
+        assert isinstance(page_size, int) and page_size > 0, "Page size must be a positive integer."
 
-        first_index, last_index = index_range(page, page_size)
         dataset = self.dataset()
+        start_index, end_index = index_range(page, page_size)
 
-        if first_index >= len(dataset):
+        if start_index >= len(dataset):
             return []
 
-        return dataset[first_index:last_index]
+        return dataset[start_index:end_index]
