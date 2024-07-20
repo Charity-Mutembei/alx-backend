a750da1b9067b4cbf86688fe8530a2763f343bcf
Charity-Mutembei <charity.softwaredev@gmail.com>
2024-07-20 16:02:17 +0300

    task 3

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
-Write a function named index_range that
-takes two integer arguments page and page_size.
+Write a function named index_range that takes two integer arguments
+page and page_size.
 """
 
 import csv
@@ -11,8 +11,7 @@ from typing import List, Dict, Union
 
 def index_range(page: int, page_size: int) -> tuple:
     """
-    Returns a tuple of size two containing a start
-    index and an end index.
+    Returns a tuple of size two containing a start index and an end index.
     """
     first_index = (page - 1) * page_size
     last_index = first_index + page_size
@@ -37,8 +36,10 @@ class Server:
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List[str]]:
         """Return the appropriate page of dataset."""
-        assert isinstance(page, int) and page > 0, "Page must be a positive integer."
-        assert isinstance(page_size, int) and page_size > 0, "Page size must be a positive integer."
+        assert isinstance(page, int) and page > 0, \
+            "Page must be a positive integer."
+        assert isinstance(page_size, int) and page_size > 0, \
+            "Page size must be a positive integer."
 
         dataset = self.dataset()
         start_index, end_index = index_range(page, page_size)
@@ -48,7 +49,8 @@ class Server:
 
         return dataset[start_index:end_index]
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Union[int, List[List[str]], None]]:
+    def get_hyper(self, page: int = 1, page_size: int = 10
+                  ) -> Dict[str, Union[int, List[List[str]], None]]:
         """Return a dictionary containing pagination details."""
         dataset = self.dataset()
         total_items = len(dataset)
