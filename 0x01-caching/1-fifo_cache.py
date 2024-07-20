70a7ee8b518d490ed1799a8b88c9e439c3ff1dd3
Charity-Mutembei <charity.softwaredev@gmail.com>
2024-07-20 16:09:54 +0300

    task 1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,36 +1,37 @@
 #!/usr/bin/python3
+""" FIFOCache module
 """
-Create a class FIFOCache that inherits
-from BaseCaching and is a caching system:
-"""
-
-BaseCaching = __import__('base_caching').BaseCaching
+from base_caching import BaseCaching
+from collections import OrderedDict
 
 
 class FIFOCache(BaseCaching):
+    """ FIFOCache inherits from BaseCaching
     """
-    inherits from BaseCaching and a caching system
-    """
+
     def __init__(self):
-        """ Initialize the FIFO cache
+        """ Initialize the FIFOCache
         """
         super().__init__()
+        self.order = OrderedDict()
 
     def put(self, key, item):
-        """ Add an item in the cache
+        """ Add an item in the cache using FIFO algorithm
         """
         if key is not None and item is not None:
-            if len(self.cache_data) >= self.MAX_ITEMS:
-                # Discard the first item (FIFO)
-                discarded_key = next(iter(self.cache_data))
-                del self.cache_data[discarded_key]
-                print("DISCARD: {}".format(discarded_key))
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                # Get the first item to discard (FIFO)
+                oldest_key, _ = self.order.popitem(last=False)
+                self.cache_data.pop(oldest_key)
+                print(f"DISCARD: {oldest_key}")
 
+            # Add the item to cache_data and order
             self.cache_data[key] = item
+            self.order[key] = None
 
     def get(self, key):
         """ Get an item by key
         """
-        if key is not None:
-            return self.cache_data.get(key)
-        return None
+        if key is None:
+            return None
+        return self.cache_data.get(key, None)
